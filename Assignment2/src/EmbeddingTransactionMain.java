b35ef04721a0c4a39b8d0a953e3e1640db2620c0
Harpreet Singh <hr557871@dal.ca>
2020-10-23 22:48:43 -0300

    Main class for executing embedded transactions

---
+++ Assignment2/src/EmbeddingTransactionMain.java
@@ -0,0 +1,25 @@
+public class EmbeddingTransactionMain {
+    public static void main(String[] args) {
+        EmbeddedTransactions.sequence();
+        EmbeddedTransactions transaction1 = new EmbeddedTransactions();
+        EmbeddedTransactions transaction2 = new EmbeddedTransactions();
+
+        transaction1.connect();
+        transaction2.connect();
+
+        transaction1.performQuery(0);
+        transaction2.performQuery(0);
+
+        transaction1.performQuery(1);
+        transaction2.performQuery(1);
+
+        transaction1.performQuery(2);
+        transaction2.performQuery(3);
+
+        transaction1.performQuery(4);
+        transaction2.performQuery(4);
+
+        transaction1.closeConnection();
+        transaction2.closeConnection();
+    }
+}
